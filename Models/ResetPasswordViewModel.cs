7bc14725030a9098081608a56d9f76188efff2cb
siranjeevicad01 <siranjeevicad01@gmail.com>
2024-04-08 21:20:17 +0530

    rectify error in model

--- Models/ResetPasswordViewModel.cs
+++ Models/ResetPasswordViewModel.cs
@@ -9,7 +9,7 @@ namespace pandafilm.Models
 		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
 		[DataType(DataType.Password)]
 		[Display(Name = "Password")]
-		public string Password { get; set; }
+		public string? Password { get; set; }
 		[Required]
 		[DataType(DataType.Password)]
 		[Compare("Password", ErrorMessage = "Must be same as password")]
